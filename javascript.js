ad420a50bf07f2f16cfe7bd04fcc9663b16a8bff
NoWa-FTN <134709569+NoWa-FTN@users.noreply.github.com>
2024-04-04 18:03:13 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,5 +1,41 @@
 console.log('Bonjour');
 
+// Fonction pour activer le mode sombre
+function enableDarkMode() {
+  document.body.classList.add('dark-theme'); // Ajoute la classe dark-theme au corps du document
+  localStorage.setItem('theme', 'dark'); // Enregistre le thème sombre dans le stockage local
+}
+
+// Fonction pour désactiver le mode sombre
+function disableDarkMode() {
+  document.body.classList.remove('dark-theme'); // Supprime la classe dark-theme du corps du document
+  localStorage.setItem('theme', 'light'); // Enregistre le thème clair dans le stockage local
+}
+
+// Fonction pour vérifier le thème stocké localement et appliquer le thème approprié
+function checkTheme() {
+  const theme = localStorage.getItem('theme'); // Récupère le thème depuis le stockage local
+  if (theme === 'dark') {
+      enableDarkMode(); // Active le mode sombre si le thème est sombre
+      document.getElementById('themeSwitch').checked = true; // Coche l'interrupteur si le thème est sombre
+  } else {
+      disableDarkMode(); // Désactive le mode sombre sinon
+      document.getElementById('themeSwitch').checked = false; // Décoche l'interrupteur si le thème est clair
+  }
+}
+
+// Appel de la fonction pour vérifier le thème au chargement de la page
+checkTheme();
+
+// Écouteur d'événements pour détecter le changement de l'interrupteur du thème
+document.getElementById('themeSwitch').addEventListener('change', function() {
+  // Vérifie l'état actuel de l'interrupteur
+  if (this.checked) {
+      enableDarkMode(); // Active le mode sombre si l'interrupteur est coché
+  } else {
+      disableDarkMode(); // Désactive le mode sombre sinon
+  }
+});
 
 window.addEventListener('scroll', function() {
   var scrollPosition = window.pageYOffset;
